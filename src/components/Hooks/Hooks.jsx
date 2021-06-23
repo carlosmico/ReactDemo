fba6c526f66c10d98625cebd3b6dad0dc3846cd5
Carlos Micó <c.mico@geekshubs.com>
2021-06-23 11:50:35 +0200

    UseEffect added

--- src/components/Hooks/Hooks.jsx
+++ src/components/Hooks/Hooks.jsx
@@ -1,8 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Hooks.scss";
 
 const Hooks = () => {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  let number = 1;
+
+  //Mount or Update
+  useEffect(() => console.log("Mounted or updated"));
+
+  //Mount
+  useEffect(() => {
+    console.log("Mounted");
+    setTimeout(() => setText("Hola"), 3000);
+    setTimeout(() => (number = 34), 2000);
+  }, []);
+
+  //Update count
+  useEffect(() => {
+    console.log("Count updated: ", count);
+  }, [count]);
+
+  //Update text
+  useEffect(() => {
+    console.log("Text updated: ", text);
+  }, [text]);
+
+  //Update count or text
+  useEffect(() => {
+    console.log("Count or text updated");
+  }, [count, text]);
+
+  //Update common var
+  useEffect(() => {
+    console.log("Common var updated: ", number);
+  }, [number]);
 
   const updateCount = (operation) => {
     const num = 1;
